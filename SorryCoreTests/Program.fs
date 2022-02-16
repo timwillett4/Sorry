a31d75d080db6cc98044f6e181259d7418f0afa8
Tim Willett <timwillett4@gmail.com>
2022-02-15 23:41:20 -0800

    Change test project to run all test in assembly

--- SorryCoreTests/Program.fs
+++ SorryCoreTests/Program.fs
@@ -1,14 +1,8 @@
-﻿open Sorry.Core.Tests.CreateGameTests
+﻿open Sorry.Core.Tests
+open Sorry.Core.Tests.CreateGameTests
 open Expecto
 open System
 
 [<EntryPoint>]
 let main argv =
-    let filter = 
-        match argv with
-        | [|_;filter|] -> fun (testName:string) -> testName.Contains filter
-        | _ -> fun _ -> true
-
-    createGameTests
-    |> Test.filter filter
-    |> runTests defaultConfig
+    runTestsInAssembly defaultConfig argv 
